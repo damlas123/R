d3bc146685fcc147dbf92a9c70cf09ae7ae9f99f
Damla Söylemez <damlasoylemez321@gmail.com>
2025-11-05 20:01:41 +0100

    study

---
+++ machine learning/lineerregression/train_testdata.R
@@ -0,0 +1,29 @@
+data<-read.csv("C:\\Users\\damla\\OneDrive\\Belgeler\\GitHub\\R\\machine learning\\lineerregression\\top_women_chess_players_aug_2020.csv")
+show(data)
+data<-na.omit(data)
+names(data)
+View(data)
+df <- data[c("Standard_Rating", "Rapid_rating")]
+set.seed(6)
+samp<-sample(1:nrow(df),size=0.8*nrow(df))
+traindata<-df[samp,]
+testdata<-df[-samp,]
+nrow(testdata)
+nrow(traindata)
+library(outliers)
+install.packages("outliers")
+score<-scores(traindata,type="z",prob=0.95)
+function()
+anytrue<-apply(score,1,FUN=function(x){any(x == TRUE)})
+index<-which(anytrue)
+traindataremoved<-traindata[-index,]
+nrow(traindata)
+nrow(traindataremoved)
+install.packages("ggplot2")
+library(ggplot2)
+fig<-ggplot(data=traindataremoved,aes(x=Rapid_rating,y=Standard_Rating))+geom_point(size=2)
+fig
+install.packages("mice")
+library("mice")
+md.pattern(traindata)
+md.pattern(data)
